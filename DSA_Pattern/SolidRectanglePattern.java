565823460ed830dddf763f7118b99cd6f28ae813
Abhishek Sharma <abhisheksharmabca2020@gmail.com>
2024-01-02 12:37:28 +0530

    Solid Rectangle Pattern

---
+++ DSA_Pattern/SolidRectanglePattern.java
@@ -0,0 +1,23 @@
+package DSA_Pattern;
+
+import java.util.Scanner;
+
+public class SolidRectanglePattern {
+public static void main(String args[]) {
+	int lenght;
+	int breath;
+	System.out.println("Enter the length of the rectange");
+	Scanner sc=new Scanner(System.in);
+	    lenght=sc.nextInt();
+	    System.out.println("Enter the breath of the rectange");
+	    breath=sc.nextInt();
+      
+      for(int i=0;i<breath;i++) {
+    	  for(int j=0;j<lenght;j++) {
+    		 
+    	  System.out.print("* ");
+    	  }
+    	  System.out.println();
+      }
+}
+}
