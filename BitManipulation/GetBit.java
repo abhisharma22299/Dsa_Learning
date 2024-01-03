552cb9a6394bdabf92cde3a0f6137ae340e79dc5
Abhishek Sharma <abhisheksharmabca2020@gmail.com>
2024-01-04 00:00:33 +0530

    Pattern

--- BitManipulation/GetBit.java
+++ BitManipulation/GetBit.java
@@ -4,7 +4,8 @@ public class GetBit {
 	// main class
 		public static void main(String args[]) {
 			// program for check binary places is zero or one 
-			
+			//  (5)binary representation is 01<-01 if we check which bit is present ta
+			//    the second number s     
 			int binaryNumber =5 ;// 0101;
 			int position=3;
 			int bitMask=1 << position;
---
+++ SortingTechnique/BubbleSorting.java
@@ -0,0 +1,17 @@
+package SortingTechnique;
+
+public class BubbleSorting {
+	public static void main(String args[]) {
+		// program to bubble  sort the	
+		// 7 8 3 1 2 //  7 3 1 2 8  //3 1 2 7 8 //  1 2 3 7 8   
+		 int[] arr= {7,8,3,1,2};
+		 for (int i=0;i<arr.length;i++ ) {
+			 for(int j=i+1;j<arr.length;j++) {
+				 if(arr[i]>arr[j]) {
+					 
+				 }
+			 }
+		 }
+	
+	}
+}
