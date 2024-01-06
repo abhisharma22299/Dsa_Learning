e10f371fa10e27efc105974e68d5f975ff7ee445
Abhishek Sharma <abhisheksharmabca2020@gmail.com>
2024-01-06 17:11:49 +0530

    dsa

---
+++ Recursion/FibnoacciSeries.java
@@ -0,0 +1,26 @@
+package Recursion;
+
+public class FibnoacciSeries {
+private static void fibonacciSeries(int a , int b , int result,int i) {
+		
+		if(i==11) {
+			return;
+		}
+		
+		result=a+b;
+		a=b;
+		b=result;
+		System.out.print(result+" ");
+		fibonacciSeries(a,b,0,i+1);
+		
+	}
+	public static void main(String[] args) {
+		// 
+		System.out.print(" 0 1 ");
+		fibonacciSeries(0,1,0,3);
+		
+	}
+
+	
+
+}
