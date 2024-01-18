c360e8af86d89a4c5ec261c385cc1e30559fc6b6
AbhiSharma22299 <abhisheksharmabca2020@gmail.com>
2024-01-19 00:32:59 +0530

    DSA

---
+++ TileProblemWithRecursion/PlaceTiles.java
@@ -0,0 +1,27 @@
+package TileProblemWithRecursion;
+
+public class PlaceTiles {
+public static int placeTiles(int n,int m) {
+	
+	if(n==m) {
+		return 2;
+		
+	}
+	if(n<m) {
+		return 1;
+	}
+	//vertical
+	int verticalPlace=placeTiles(n-m, m);
+	
+	//horizonatal
+	int horizontalPlace=placeTiles(n-1, m);
+	
+	return horizontalPlace+verticalPlace;
+}
+	
+	
+	
+	public static void main(String[] args) {
+System.out.println(placeTiles(4, 2));
+	}
+}
