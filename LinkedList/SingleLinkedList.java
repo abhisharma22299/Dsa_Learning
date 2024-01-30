cd61f0cf00daf702b489744f0a22398a68d3690b
AbhiSharma22299 <abhisheksharmabca2020@gmail.com>
2024-01-31 00:03:06 +0530

    JDBC

--- LinkedList/SingleLinkedList.java
+++ LinkedList/SingleLinkedList.java
@@ -1,64 +1,102 @@
 package LinkedList;
-
-public class SingleLinkedList {
+class SingleLinkedList{
+	
+	Node head;
 	class Node{
-		String Data;
+		int data;
 		Node next;
+		Node (int d){
+			data=d;
+			next=null;
+		}
+	}
+	
+	
+	// implementation of insertion of a node at end
+	
+	public void insertAtEnd(int newData) {
+		Node newNode=new Node(newData);
+		// checking linkedList is empty or not 
+	
+	}
+	public static void main(String[] args) {
+			
 		
-		
-		public Node(String data) {
+	}
+}
 
-			this.Data = data;
-			this.next = next;
-		}
-		
-	}
-	
-	
-	// add first
-	
-	public static void addFirst(String data) {
-		Node newNode=new Node(data);
-		if(head == null) {
-			head=newNode;
-		}
-		newNode.next=head;
-		head=newNode;
-		
-	}
-	// add Last
-	
-		public static void addLast(String data) {
-			Node newNode=new Node(data);
-			if(head == null) {
-				head=newNode;
-			}
-			Node currNode=head;
-			while(currNode!= null) {
-				currNode=currNode.next;
-			}
-			currNode.next=newNode;
-		}
-		
-		// print
-		public static void printLinkedList() {
-			Node currNode=head;
-			if(head==null) {
-				System.out.println("List is empty");
-				return;
-			}
-			while(currNode !=null) {
-			System.out.print(currNode.Data+" -> ");
-				currNode = currNode.next;
-				
-			}
-			currNode.next=newNode;
-			System.out.println("null");
-		}
-public static void main(String[] args) {
-	SingleLinkedList sll=new SingleLinkedList();
-	sll.addFirst(" A ");
-	sll.addFirst(" IS ");
-	sll.printLinkedList();
-}
-}
+
+
+
+
+
+
+
+
+
+
+
+//public class SingleLinkedList {
+//	
+//	static Node head;
+//	class Node{
+//		String Data;
+//		Node next;
+//		
+//		
+//		public Node(String data) {
+//
+//			this.Data = data;
+//			this.next = next;
+//		}
+//		
+//	}
+//	
+//	
+//	// add first
+//	
+//	public static void addFirst(String data) {
+//		Node newNode=new Node(data);
+//		if(head == null) {
+//			head=newNode;
+//		}
+//		newNode.next=head;
+//		head=newNode;
+//		
+//	}
+//	// add Last
+//	
+//		public static void addLast(String data) {
+//			Node newNode=new Node(data);
+//			if(head == null) {
+//				head=newNode;
+//			}
+//			Node currNode=head;
+//			while(currNode!= null) {
+//				currNode=currNode.next;
+//			}
+//			currNode.next=newNode;
+//		}
+//		
+//		// print
+//		public static void printLinkedList() {
+//			Node currNode=head;
+//			if(head==null) {
+//				System.out.println("List is empty");
+//				return;
+//			}
+//			while(currNode !=null) {
+//			System.out.print(currNode.Data+" -> ");
+//				currNode = currNode.next;
+//				
+//			}
+//			currNode.next=newNode;
+//			System.out.println("null");
+//		}
+//public static void main(String[] args) {
+//	SingleLinkedList sll=new SingleLinkedList();
+//	sll.addFirst(" A ");
+//	sll.addFirst(" IS ");
+//	sll.printLinkedList();
+//}
+//}
