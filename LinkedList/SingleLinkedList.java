249d05ee7a21f65980416e177e0763b565bfbddd
AbhiSharma22299 <abhisheksharmabca2020@gmail.com>
2024-01-31 20:16:57 +0530

    DSA

--- LinkedList/SingleLinkedList.java
+++ LinkedList/SingleLinkedList.java
@@ -1,5 +1,7 @@
 package LinkedList;
 
+import Recursion.printSubsetOfNaturalNumber;
+
 class SingleLinkedList {
 
 	Node head;
@@ -24,16 +26,62 @@ class SingleLinkedList {
 			head = new Node(newData);
 			return;
 		}
+		newNode.next=null;
 		// for linked list is not empty
 		// creating a temp Node
             Node temp=head;
-		if (head != null) {
-
+		while(temp.next!=null) {
+			temp=temp.next;
 		}
+		temp.next=newNode;
+		return;
+	}
+	// implementation of insertion of a node at First
+
+	public void insertAtFirst(int newData) {
+		Node newNode = new Node(newData);
+		// checking linkedList is empty or not
+		// for linked list is empty
+		if (head == null) {
+			head = new Node(newData);
+			return;
+		}
+		//newNode.next=null;
+		// for linked list is not empty
+		// creating a temp Node
+            Node temp=newNode;
+           temp.next=head;
+           head=temp;
+		return;
 	}
 
+// implementation of the printing a linked List
+	public void printLnikedList() {
+		Node temp=head;
+		while(temp.next!=null) {
+			System.out.print(temp.data+"  ");
+			temp=temp.next;
+		}
+	}
 	public static void main(String[] args) {
-
+	SingleLinkedList list=new SingleLinkedList();
+	
+	list.insertAtEnd(2);
+	list.insertAtEnd(3);
+	list.insertAtEnd(4);
+	list.insertAtEnd(5);
+	list.insertAtEnd(6);
+	list.insertAtEnd(7);
+	list.insertAtEnd(8);
+	list.insertAtEnd(9);
+	list.printLnikedList();
+	
+	list.insertAtFirst(22);
+	System.out.println();
+	list.printLnikedList();
+list.insertAtFirst(20);
+System.out.println();
+list.printLnikedList();
 	}
 }
 
