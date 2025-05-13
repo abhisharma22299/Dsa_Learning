fd5b354cfbb938b1d3695930e520b5c6adfb2b05
Abhishek Sharma <abhisheksharmabca2020@gmail.com>
2025-05-13 22:47:14 +0530

    DSA

--- StackImplementation/JavaCollectionFrameWork.java
+++
@@ -1,8 +0,0 @@
-package StackImplementation;
-
-public class JavaCollectionFrameWork {
-
-	public static void main(String[] args) {
-		
-	}
-}
--- t.java
+++
@@ -1,9 +0,0 @@
-
-public class t {
-static {
-	
-	System.out.println("hii");
-	System.exit(0);
-}
-
-}
